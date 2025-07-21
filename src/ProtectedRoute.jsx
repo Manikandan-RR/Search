d1fc195bc74878fab0e33b74d28298bb34992efa
Manikandan-RR <rajmani1985py@gmail.com>
2025-07-21 18:34:48 +0530

    ProtectRoute

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -4,3 +4,13 @@ export const ProtectedRoute = ({ children }) => {
     const isLogin = sessionStorage.getItem('isLogin')
     return isLogin ? children : <Navigate to="/" />
 }
+
+// export const ProtectedRoute = (props) => {
+//     const isLogin = sessionStorage.getItem('isLogin')
+//     if (isLogin) {
+//         return props.children
+//     }
+//     else {
+//         return <Navigate to='/' />
+//     }
+// }
